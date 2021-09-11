393136d61c6ed93a5d1b34a6ce65e459ce7dc6ed
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-11 05:44:40 +0530

    Added: To validate and accept email

--- src/main/java/com/bridgelabz/userregistrationvalidation/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationvalidation/UserRegistration.java
@@ -7,4 +7,10 @@ public class UserRegistration {
 		String regularExpression = "[A-Z]+[a-zA-Z\\ ]{2,}";
 		return name.matches(regularExpression);
 	}
+	
+	public static boolean checkEmail(String email) {
+
+		String finalRegularExpressionToCheckEmail = "((((abc)[a-zA-Z0-9]*)||(abc[_\\+\\-\\.][a-zA-Z0-9]*))+(?<!\\.)@([0-9a-zA-Z]{1,}))\\.(([a-zA-Z]{2,}\\.[a-zA-Z]{2})||([a-zA-Z\\,]{2,}))+(?<!\\.)$";
+		return email.matches(finalRegularExpressionToCheckEmail);
+	}
 }
