461320a42b3aad1347dc61615d6abc8b837ffda4
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-12 01:09:38 +0530

    Added: To throw custom exception for Password

--- src/main/java/com/bridgelabz/userregistrationvalidation/UserRegistrationCustom.java
+++ src/main/java/com/bridgelabz/userregistrationvalidation/UserRegistrationCustom.java
@@ -46,4 +46,18 @@ public class UserRegistrationCustom {
 			throw new UserException("Invalid Phone Number", ExceptionType.PHONE_NULL);
 		}
 	}
+	public static void validatePassword(String password) throws UserException {
+
+		try {
+			String regularExpression = "^(?=.*[0-9])(?=.*[A-Z])(?=.{8,}$)[0-9a-zA-Z]*[@#$%_][0-9a-zA-Z]*$";
+			if (password.length()==0)
+				throw new UserException("Invalid Password", ExceptionType.PASSWORD_EMPTY);
+			boolean result = password.matches(regularExpression);
+			if(result == false)
+				throw new UserException("Invalid Password", ExceptionType.PASSWORD_INVALID);
+		}
+		catch(NullPointerException e) {
+			throw new UserException("Invalid Password", ExceptionType.PASSWORD_NULL);
+		}
+	}
 }
