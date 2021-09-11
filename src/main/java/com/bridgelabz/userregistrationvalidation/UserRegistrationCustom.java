aa47ddeed3b4bf673d6fad639e526516fee2b5a6
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-12 01:08:54 +0530

    Added: To throw custom exception for Email

--- src/main/java/com/bridgelabz/userregistrationvalidation/UserRegistrationCustom.java
+++ src/main/java/com/bridgelabz/userregistrationvalidation/UserRegistrationCustom.java
@@ -17,6 +17,19 @@ public class UserRegistrationCustom {
 		catch(NullPointerException e) {
 			throw new UserException("Invalid Name", ExceptionType.NAME_NULL);
 		}
+	}
+	public static void validateEmail(String email) throws UserException {
 
+		try {
+			String finalRegularExpressionTovalidateEmail = "((((abc)[a-zA-Z0-9]*)||(abc[_\\+\\-\\.][a-zA-Z0-9]*))+(?<!\\.)@([0-9a-zA-Z]{1,}))\\.(([a-zA-Z]{2,}\\.[a-zA-Z]{2})||([a-zA-Z\\,]{2,}))+(?<!\\.)$";
+			if (email.length()==0)
+				throw new UserException("Invalid Email", ExceptionType.EMAIL_EMPTY);
+			boolean result = email.matches(finalRegularExpressionTovalidateEmail);
+			if(result == false)
+				throw new UserException("Invalid Email", ExceptionType.EMAIL_INVALID);
+		}
+		catch(NullPointerException e) {
+			throw new UserException("Invalid Email", ExceptionType.EMAIL_NULL);
+		}
 	}
 }
