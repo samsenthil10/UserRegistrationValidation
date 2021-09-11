da73be8186e281b25430944c86992c35d9abd0ad
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-11 05:49:51 +0530

    Modified: To validate password with atleast one upper case

--- src/main/java/com/bridgelabz/userregistrationvalidation/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationvalidation/UserRegistration.java
@@ -1,28 +1,28 @@
 package com.bridgelabz.userregistrationvalidation;
 
 public class UserRegistration {
-	
+
 	public static boolean checkName(String name) {
 
 		String regularExpression = "[A-Z]+[a-zA-Z\\ ]{2,}";
 		return name.matches(regularExpression);
 	}
-	
+
 	public static boolean checkEmail(String email) {
 
 		String finalRegularExpressionToCheckEmail = "((((abc)[a-zA-Z0-9]*)||(abc[_\\+\\-\\.][a-zA-Z0-9]*))+(?<!\\.)@([0-9a-zA-Z]{1,}))\\.(([a-zA-Z]{2,}\\.[a-zA-Z]{2})||([a-zA-Z\\,]{2,}))+(?<!\\.)$";
 		return email.matches(finalRegularExpressionToCheckEmail);
 	}
-	
+
 	public static boolean checkPhoneNumber(String phoneNumber) {
 
 		String regularExpression = "[0-9]*\\ [1-9][0-9]{9}";
 		return phoneNumber.matches(regularExpression);
 	}
-	
+
 	public static boolean checkPassword(String password) {
 
-		String regularExpression = ".{8,}";
+		String regularExpression = "(?=.*[a-z])(?=.*[A-Z]).{8,}";
 		return password.matches(regularExpression);
 	}
 }
