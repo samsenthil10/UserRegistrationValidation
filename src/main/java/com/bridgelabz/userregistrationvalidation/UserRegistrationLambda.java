53612edef6ccf86fa8a4c8f41ba9daad8659a2a8
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-14 06:11:23 +0530

    Added: To validate email using lambda function

--- src/main/java/com/bridgelabz/userregistrationvalidation/UserRegistrationLambda.java
+++ src/main/java/com/bridgelabz/userregistrationvalidation/UserRegistrationLambda.java
@@ -23,4 +23,19 @@ public class UserRegistrationLambda {
 			throw new UserException("Invalid Name", ExceptionType.NAME_NULL);
 		}
 	};
+
+	IValidateUser validateEmail = email -> {
+
+		try {
+			String finalRegularExpressionTovalidateEmail = "((((abc)[a-zA-Z0-9]*)||(abc[_\\+\\-\\.][a-zA-Z0-9]*))+(?<!\\.)@([0-9a-zA-Z]{1,}))\\.(([a-zA-Z]{2,}\\.[a-zA-Z]{2})||([a-zA-Z\\,]{2,}))+(?<!\\.)$";
+			if (email.length()==0)
+				throw new UserException("Invalid Email", ExceptionType.EMAIL_EMPTY);
+			boolean result = email.matches(finalRegularExpressionTovalidateEmail);
+			if(result == false)
+				throw new UserException("Invalid Email", ExceptionType.EMAIL_INVALID);
+		}
+		catch(NullPointerException e) {
+			throw new UserException("Invalid Email", ExceptionType.EMAIL_NULL);
+		}
+	};
 }
