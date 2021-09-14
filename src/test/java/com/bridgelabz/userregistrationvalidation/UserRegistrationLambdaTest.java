17ea258eebb0ed9cb64a74696c06afaa284fd3c8
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-14 06:14:40 +0530

    Added: Test case to validate email using lambda function

--- src/test/java/com/bridgelabz/userregistrationvalidation/UserRegistrationLambdaTest.java
+++ src/test/java/com/bridgelabz/userregistrationvalidation/UserRegistrationLambdaTest.java
@@ -35,4 +35,62 @@ public class UserRegistrationLambdaTest {
 			Assert.assertEquals(ExceptionType.NAME_EMPTY,e.type);
 		}
 	}
+	@Test
+	public void givenLastName_WhenNull_ShouldReturnNull() {
+		UserRegistrationLambda userRegistration = new UserRegistrationLambda();
+		try {
+			userRegistration.validateName.validate(null);
+		}
+		catch(UserException e) {
+			Assert.assertEquals(ExceptionType.NAME_NULL,e.type);
+		}
+	}
+	@Test
+	public void givenLastName_WhenInvalid_ShouldReturnInvalid() {
+		UserRegistrationLambda userRegistration = new UserRegistrationLambda();
+		try {
+			userRegistration.validateName.validate("Ov");
+		} catch (UserException e) {
+			Assert.assertEquals(ExceptionType.NAME_INVALID,e.type);
+		}
+	}
+	@Test
+	public void givenLastName_WhenEmpty_ShouldReturnEmpty() {
+		UserRegistrationLambda userRegistration = new UserRegistrationLambda();
+		try {
+			userRegistration.validateName.validate("");
+		} catch (UserException e) {
+			Assert.assertEquals(ExceptionType.NAME_EMPTY,e.type);
+		}
+	}
+	@Test
+	public void givenEmail_WhenNull_ShouldReturnNull() {
+		UserRegistrationLambda userRegistration = new UserRegistrationLambda();
+		try {
+			userRegistration.validateEmail.validate(null);
+		} catch (UserException e) {
+
+			Assert.assertEquals(ExceptionType.EMAIL_NULL,e.type);
+		}
+	}
+	@Test
+	public void givenEmail_WhenInvalid_ShouldReturnInvalid() {
+		UserRegistrationLambda userRegistration = new UserRegistrationLambda();
+		try {
+			userRegistration.validateEmail.validate("abc.com");
+		} catch (UserException e) {
+
+			Assert.assertEquals(ExceptionType.EMAIL_INVALID,e.type);
+		}
+	}
+	@Test
+	public void givenEmail_WhenEmpty_ShouldReturnEmpty() {
+		UserRegistrationLambda userRegistration = new UserRegistrationLambda();
+		try {
+			userRegistration.validateEmail.validate("");
+		} catch (UserException e) {
+
+			Assert.assertEquals(ExceptionType.EMAIL_EMPTY,e.type);
+		}
+	}
 }
